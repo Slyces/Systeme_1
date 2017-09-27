d4351174a8820c0f93922a1c76a8f6bba9821ca3
Slyces <slyces.coding@gmail.com>
2017-09-27 20:18:38 +0200

    Action 4.2 -- compile

--- code/test/putchar.c
+++ code/test/putchar.c
@@ -2,17 +2,17 @@
 
 void print(char c, int n)
 {
-  int i ;
- #if 0
-  for (i = 0; i < n; i++) {
-    PutChar(c + i);
-  }
-  PutChar('\n');
+        int i;
+ #if 1
+        for (i = 0; i < n; i++) {
+                PutChar(c + i);
+        }
+        PutChar('\n');
  #endif
 }
 
 int main()
 {
-  print('a',4);
-  Halt();
+        print('a',4);
+        Halt();
 }
--- code/userprog/synchconsole.cc
+++ code/userprog/synchconsole.cc
@@ -4,53 +4,39 @@
   #include "synchconsole.h"
   #include "synch.h"
 
-static Semaphore *readAvail;
-static Semaphore *writeDone;
+  static Semaphore *readAvail;
+  static Semaphore *writeDone;
 
-static void ReadAvailHandler(void *arg) {
-    (void)arg;
-    readAvail->V();
-}
+  static void ReadAvailHandler(void *arg) { (void) arg; readAvail->V(); }
+  static void WriteDoneHandler(void *arg) { (void) arg; writeDone->V(); }
 
-static void WriteDoneHandler(void *arg) {
-    (void)arg;
-    writeDone->V();
-}
-
-SynchConsole::SynchConsole(const char *in, const char *out) {
+  SynchConsole::SynchConsole(const char *in, const char *out) {
     readAvail = new Semaphore("read avail", 0);
     writeDone = new Semaphore("write done", 0);
-    console   = new Console(in, out, ReadAvailHandler, WriteDoneHandler, 0);
-}
-
-SynchConsole::~SynchConsole() {
+    console = new Console (in, out, ReadAvailHandler, WriteDoneHandler, 0);
+  }
+  SynchConsole::~SynchConsole() {
     delete console;
     delete writeDone;
     delete readAvail;
-}
-
-void SynchConsole::SynchPutChar(int ch) {
-    console->PutChar(ch);
-    writeDone->P();
-}
-
-// @TODO vérifier mon erreur
-int SynchConsole::SynchGetChar() {
-    readAvail->P(); // wait for character to arrive
-    return console->GetChar();
-}
-
-void SynchConsole::SynchPutString(const char *s) {
+  }
+  void SynchConsole::SynchPutChar(int ch) {
+    console->PutChar (ch);
+    writeDone->P ();
+  }
+  // @TODO vérifier mon erreur
+  int SynchConsole::SynchGetChar() {
+    readAvail->P ();	// wait for character to arrive
+    return console -> GetChar ();
+  }
+  void SynchConsole::SynchPutString(const char s[]) {
     for (unsigned int i = 0; i < sizeof(s); i++) {
-        SynchPutChar(s[i]);
+      SynchPutChar(s[i]);
     }
-}
-
+  }
   #if 0
-void SynchConsole::SynchGetString(char *s, int n) {
+  void SynchConsole::SynchGetString(char *s, int n) {
     //
-}
-
-  #endif // if 0
-
-#endif   // CHANGED
+  }
+  #endif
+#endif // CHANGED
