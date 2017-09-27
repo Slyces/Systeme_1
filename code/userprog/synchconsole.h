4b0308f3ba20461a762fbd4e70078c0ccaba3a9a
Slyces <slyces.coding@gmail.com>
2017-09-27 20:08:47 +0200

    Action 4.1
    Ne se compile pas encore

--- code/userprog/synchconsole.h
+++ code/userprog/synchconsole.h
@@ -1,25 +1,32 @@
 #include "console.h"
 
 #ifdef CHANGED
-  #ifndef SYNCHCONSOLE_H
+  # ifndef SYNCHCONSOLE_H
 
-    #define SYNCHCONSOLE_H
+    #  define SYNCHCONSOLE_H
 
-    #include "copyright.h"
-    #include "utility.h"
-    #include "console.h"
+    #  include "copyright.h"
+    #  include "utility.h"
+    #  include "console.h"
 
 class SynchConsole : dontcopythis {
 public:
-SynchConsole(const char *readFile, const char *writeFile);
-// initialize the hardware console device
-~SynchConsole();                                    // clean up console emulation
-void SynchPutChar(int ch);                          // Unix putchar(3S)
-int SynchGetChar();                                 // Unix getchar(3S)
-void SynchPutString(const char *s);                 // Unix fputs(3S)
-void SynchGetString(char *s, int n);                       // Unix fgets(3S)
+
+    SynchConsole(const char *readFile,
+                 const char *writeFile);
+
+    // initialize the hardware console device
+    ~SynchConsole();                    // clean up console emulation
+    void SynchPutChar(int ch);          // Unix putchar(3S)
+    int  SynchGetChar();                // Unix getchar(3S)
+    void SynchPutString(const char *s); // Unix fputs(3S)
+    void SynchGetString(char *s,
+                        int   n);       // Unix fgets(3S)
+
 private:
-Console *console;
+
+    Console *console;
 };
-  #endif // SYNCHCONSOLE_H
-#endif // CHANGED
+
+  # endif // SYNCHCONSOLE_H
+#endif    // CHANGED
