7a69878b6bc051265ef6b1f9cfdef92e95435e45
Slyces <slyces.coding@gmail.com>
2017-09-28 08:58:05 +0200

    Tutorat const char *

--- code/userprog/synchconsole.cc
+++ code/userprog/synchconsole.cc
@@ -4,39 +4,64 @@
   #include "synchconsole.h"
   #include "synch.h"
 
-  static Semaphore *readAvail;
-  static Semaphore *writeDone;
+static Semaphore *readAvail;
+static Semaphore *writeDone;
 
-  static void ReadAvailHandler(void *arg) { (void) arg; readAvail->V(); }
-  static void WriteDoneHandler(void *arg) { (void) arg; writeDone->V(); }
+static void ReadAvailHandler(void *arg) {
+    (void)arg; readAvail->V();
+}
 
-  SynchConsole::SynchConsole(const char *in, const char *out) {
+static void WriteDoneHandler(void *arg) {
+    (void)arg; writeDone->V();
+}
+
+SynchConsole::SynchConsole(const char *in, const char *out) {
     readAvail = new Semaphore("read avail", 0);
     writeDone = new Semaphore("write done", 0);
-    console = new Console (in, out, ReadAvailHandler, WriteDoneHandler, 0);
-  }
-  SynchConsole::~SynchConsole() {
+    console   = new Console(in, out, ReadAvailHandler, WriteDoneHandler, 0);
+}
+
+SynchConsole::~SynchConsole() {
     delete console;
     delete writeDone;
     delete readAvail;
-  }
-  void SynchConsole::SynchPutChar(int ch) {
-    console->PutChar (ch);
-    writeDone->P ();
-  }
-  // @TODO vérifier mon erreur
-  int SynchConsole::SynchGetChar() {
-    readAvail->P ();	// wait for character to arrive
-    return console -> GetChar ();
-  }
-  void SynchConsole::SynchPutString(const char s[]) {
-    for (unsigned int i = 0; i < sizeof(s); i++) {
-      SynchPutChar(s[i]);
+}
+
+void SynchConsole::SynchPutChar(int ch) {
+    console->PutChar(ch);
+    writeDone->P();
+}
+
+// @TODO vérifier mon erreur
+int SynchConsole::SynchGetChar() {
+    readAvail->P(); // wait for character to arrive
+    return console->GetChar();
+}
+
+void SynchConsole::SynchPutString(const char *s) {
+    // for (unsigned int i = 0; i < sizeof(s); i++) {
+    //     SynchPutChar(s[i]);
+    // }
+    int i = 0;
+
+    while (s[i] != '\0') {
+        SynchPutChar(s[i]);
+        i++;
     }
-  }
-  #if 0
-  void SynchConsole::SynchGetString(char *s, int n) {
-    //
-  }
-  #endif
-#endif // CHANGED
+}
+
+  #if 1
+void SynchConsole::SynchGetString(char *s, int n) {
+    int  i = 0;
+    char c;
+
+    while (i < n && c != EOF && c != '\n') {
+        c    = SynchGetChar();
+        s[i] = c;
+        i++;
+    }
+    s[i] = '\0';
+}
+
+  #endif // if 0
+#endif   // CHANGED
